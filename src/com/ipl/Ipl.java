9abb21ac05611cb2d9f15fa68d38932f3861189f
Jaguar-188 <ruturajlokhande9@gmail.com>
2021-08-10 23:27:54 +0530

    first commit

---
+++ src/com/ipl/Ipl.java
@@ -0,0 +1,21 @@
+package com.ipl;
+import com.ipl.model.*;
+import java.util.*;
+
+public class Ipl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+	}
+	
+	public List<Match> getMatchData(){
+		
+		
+		
+		return null;
+	}
+
+}
